69ab9b8b363316ff5e41067d7720bdcd38d173ef
Preston Neal <mrprestonneal@gmail.com>
2024-01-21 11:18:42 -0800

    add new export format

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from datetime import datetime
 import random
 import re
-from sqlite3 import Date
 from time import perf_counter
 from csv import writer, DictWriter
 from tkinter import BOTH, END, LEFT, Canvas, LabelFrame, Listbox, Scrollbar, StringVar, Text, Tk, Button, Label, Entry, Toplevel, Menu, Frame, SUNKEN, N, S, E, W, BOTTOM, X, messagebox
 from tkinter.ttk import Combobox
-from tkcalendar import Calendar, DateEntry
 from modules.commentscraper import scrape_comments
 from modules.pdfscanner import scan_for_keywords, scan_for_comments, scan_pdf_for_summary, MedicalRecord
-from modules.docwriter import generate_chronological_medical_summary, generate_tablular_medical_summary
+from modules.docwriter import generate_chronological_medical_summary, generate_tablular_medical_summary, generate_tablular_medical_summary_v2
 from pathlib import Path
-import subprocess
-import requests
 
 from configparser import ConfigParser
-import json
 
 from helpers import write_csv, get_file_path, get_save_path, get_age, get_age_at_onset
 
 config = ConfigParser()
+
 
 class App(Tk):
     def __init__(self, *args, **kwargs) -> None:
@@ -31,10 +27,10 @@ class App(Tk):
         self.status_bar = StatusBar(self)
         self.status_bar.pack(side=BOTTOM, fill=X)
 
-        self.menubar = MenuBar(self)        
+        self.menubar = MenuBar(self)
         self.config(menu=self.menubar)
-        
-        self._center()
+
+        # self._center()
         self.attributes('-alpha', 1.0)
 
     def _center(self):
@@ -54,6 +50,7 @@ class App(Tk):
         self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
         self.deiconify()
 
+
 class SummaryForm(Toplevel):
     def __init__(self, parent):
         super().__init__(parent, padx=15, pady=15)
@@ -64,7 +61,7 @@ class SummaryForm(Toplevel):
         self.pdf_path = ""
         self.attributes('-alpha', 0.0)
         self.title('New Summary')
-        
+
         self.detect_client_btn = Button(self, text="Get Data from PDF", command=self._get_summary_data)
         self.detect_client_btn.bind("<Enter>", on_enter)
         self.detect_client_btn.bind("<Leave>", on_leave)
@@ -77,19 +74,19 @@ class SummaryForm(Toplevel):
 
         self.name_label = Label(self, text="Client Name:")
         self.name_label.grid(column=0, row=1, sticky='w', padx=10, pady=2)
-        
+
         self.name_entry = Entry(self, bd=1, width=32)
         self.name_entry.grid(column=1, row=1, sticky='e', padx=10, pady=2)
-        
+
         self.ssn_label = Label(self, text="SSN:")
         self.ssn_label.grid(column=0, row=2, sticky='w', padx=10, pady=2)
-        
+
         self.ssn_entry = Entry(self, bd=1, width=32)
         self.ssn_entry.grid(column=1, row=2, sticky='e', padx=10, pady=2)
 
         self.title_label = Label(self, text="Title:")
         self.title_label.grid(column=0, row=3, sticky='w', padx=10, pady=2)
-        
+
         self.title_entry = Entry(self, bd=1, width=32)
         self.title_entry.grid(column=1, row=3, sticky='e', padx=10, pady=2)
 
@@ -161,7 +158,7 @@ class SummaryForm(Toplevel):
         self.add_work_history_btn.bind("<Enter>", on_enter)
         self.add_work_history_btn.bind("<Leave>", on_leave)
         self.add_work_history_btn.grid(column=2, row=0, columnspan=2)
-        
+
         self.work_history_frame = LabelFrame(self, text="Work History")
         self.work_history_frame.grid(column=2, row=1, columnspan=2, rowspan=12, sticky='n', pady=10)
 
@@ -203,15 +200,15 @@ class SummaryForm(Toplevel):
     def _scan_for_comments(self):
         if not self.pdf_path:
             self.pdf_path = get_file_path()
-        
+
         self.pdf_comments = scan_for_comments(self.pdf_path)
-        
+
         comment_count = len(self.pdf_comments)
         self.comment_count_label.configure(text=f'Comments Found: {comment_count}')
         #self.view_comments_btn.configure(state='normal')
         #self.view_comments_btn.bind("<Enter>", on_enter)
         #self.view_comments_btn.bind("<Leave>", on_leave)
-        
+
         self.update()
         self.deiconify()
 
@@ -222,7 +219,7 @@ class SummaryForm(Toplevel):
         self.medical_record = scan_pdf_for_summary(self.pdf_path)
         self._fill_entry_fields()
         self.update()
-        
+
     def _fill_entry_fields(self):
         mr = self.medical_record
         self.work_history = mr.claimant.work_history
@@ -243,7 +240,7 @@ class SummaryForm(Toplevel):
         self.comment_viewer = CommentViewer(self)
 
     def _save_to_file(self):
-        
+
         client_name = self.name_entry.get()
         client_ssn = self.ssn_entry.get()
         title = self.title_entry.get()
@@ -264,7 +261,7 @@ class SummaryForm(Toplevel):
 
         if self.medical_record:
             age = self.medical_record.claimant.age()
-        else: 
+        else:
             age = get_age(birthdate)
 
         if self.medical_record:
@@ -290,16 +287,26 @@ class SummaryForm(Toplevel):
             'overview': overview,
             'claimant': claimant,
             'exhibits': exhibits,
-            'pages': pages
+            'pages': pages,
+            'impairments': [
+                "Schizoaffective disorder",
+                "Deep vein thrombosis",
+                "Status/post lumbar fusion",
+                "Osteoarthritis of left knee",
+                "Cervical spondylosis",
+                "Obesity",
+            ]
         }
 
-        doc = generate_tablular_medical_summary(data)
+        # allow to choose generation template?
+        doc = generate_tablular_medical_summary_v2(data)
         output_path = get_save_path('docx')
         doc.save(output_path)
 
         self.parent.parent.status_bar._set_status('Data Saved')
         self.destroy()
         return
+
 
 class CommentViewer(Toplevel):
     def __init__(self, parent):
@@ -322,7 +329,7 @@ class CommentViewer(Toplevel):
 
         self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        
+
         for row, comment in enumerate(self.parent.pdf_comments):
             txtbox = Label(self.scrollable_frame, wraplength=500, justify=LEFT, bg="white", borderwidth=1)
             txtbox.configure(text=comment['text'])
@@ -332,12 +339,13 @@ class CommentViewer(Toplevel):
         self.container.pack()
         self.canvas.pack(side='left', fill="both", expand=True)
         self.scrollbar.pack(side="right", fill="y")
-        
+
         self.close_btn = Button(self, text='Close', width=12, command=self.destroy)
         self.close_btn.pack()
 
         center(self)
         self.attributes('-alpha', 1.0)
+
 
 class WorkHistoryForm(Toplevel):
     def __init__(self, parent):
@@ -380,7 +388,7 @@ class WorkHistoryForm(Toplevel):
 
         center(self)
         self.attributes('-alpha', 1.0)
-    
+
     def _add_entry(self):
         entry = {
             'job_title': self.job_title_entry.get(),
@@ -391,6 +399,7 @@ class WorkHistoryForm(Toplevel):
         self.parent.work_history.append(entry)
         self.parent._paint_work_history()
         self.parent.update()
+
 
 class KeywordSettingsForm(Toplevel):
     def __init__(self, parent):
@@ -427,7 +436,7 @@ class KeywordSettingsForm(Toplevel):
 
         self.rightFrame = Frame(self)
         self.rightFrame.grid(column=1, row=1, pady=10)
-        
+
         self._get_keywords()
 
         self.keywordboxlabel = Label(self.rightFrame, text='Active Keywords')
@@ -447,12 +456,12 @@ class KeywordSettingsForm(Toplevel):
         self.deleteBtn.grid(column=0, row=2, pady=10)
         self.deleteBtn.bind("<Enter>", on_enter)
         self.deleteBtn.bind("<Leave>", on_leave)
-        
+
         doneBtn = Button(self, text='Done', command=self.destroy)
         doneBtn.bind("<Enter>", on_enter)
         doneBtn.bind("<Leave>", on_leave)
         doneBtn.grid(column=0, row=3, columnspan=2, pady=10)
-     
+
         center(self)
         self.attributes('-alpha', 1.0)
 
@@ -478,7 +487,6 @@ class KeywordSettingsForm(Toplevel):
     def _select_keywords_file(self):
         custom_keywords_path = get_file_path()
 
-
     def _refresh_keyword_listbox(self):
         self.listbox.delete(0, END)
         self._get_keywords()
@@ -497,6 +505,7 @@ class KeywordSettingsForm(Toplevel):
             keyword_file.writelines(kw + '\n' for kw in self.keywords)
         self.listbox.delete(idx)
 
+
 class ContextSizeForm(Toplevel):
     def __init__(self, parent):
         super().__init__(parent, padx=15, pady=15)
@@ -505,13 +514,13 @@ class ContextSizeForm(Toplevel):
 
         words_minus_label = Label(self, text="Words Before Keyword:")
         words_minus_label.grid(column=0, row=0, sticky='w', padx=10, pady=3)
-        
+
         self.words_minus_entry = Entry(self, width=3, bd=1)
         self.words_minus_entry.grid(column=1, row=0, sticky='e', padx=10, pady=3)
-        
+
         words_plus_label = Label(self, text="Words After Keyword:")
         words_plus_label.grid(column=0, row=1, sticky='w', padx=10, pady=3)
-        
+
         self.words_plus_entry = Entry(self, width=3, bd=1)
         self.words_plus_entry.grid(column=1, row=1, sticky='e', padx=10, pady=3)
 
@@ -519,7 +528,7 @@ class ContextSizeForm(Toplevel):
         done_btn.bind("<Enter>", on_enter)
         done_btn.bind("<Leave>", on_leave)
         done_btn.grid(column=0, row=3, columnspan=2, pady=3)
-        
+
         self._get_context_settings()
         center(self)
         self.attributes('-alpha', 1.0)
@@ -539,15 +548,16 @@ class ContextSizeForm(Toplevel):
         with open('./config/config.ini', 'w') as configfile:
             config.write(configfile)
 
+
 class StatusBar(Frame):
-    
+
     def __init__(self, parent):
         super().__init__(parent, bd=1, relief=SUNKEN)
         self.parent = parent
-                
+
         self.status = Label(self, text='Ready', anchor=W, padx=5)
         self.status.grid(column=0, row=0, sticky='w', columnspan=1)
-        
+
         self.version = Label(self, text='error', anchor=E, padx=5)
         self.version.grid(column=1, row=0, sticky="e", columnspan=1)
         self._get_app_version()
@@ -559,10 +569,11 @@ class StatusBar(Frame):
 
     def _reset_status(self):
         self.status.configure(text='Ready')
-    
+
     def _get_app_version(self):
         version = get_app_version()
         self.version.config(text=f"v{version}")
+
 
 class MenuBar(Menu):
     def __init__(self, parent) -> None:
@@ -571,7 +582,6 @@ class MenuBar(Menu):
 
         self.filemenu = Menu(self, tearoff=0)
         self.filemenu.add_command(label='New Summary', command=self._new_summary_form)
-        self.filemenu.add_command(label='Check for Updates', command=self._check_for_updates)
         self.filemenu.add_command(label="Exit", command=self.parent.destroy)
         self.add_cascade(label="File", menu=self.filemenu)
 
@@ -579,24 +589,17 @@ class MenuBar(Menu):
         self.actions_menu.add_command(label="Scan PDF for Keywords", command=self._run_keyword_scan)
         self.actions_menu.add_command(label="Scrape Comments from PDF", command=self._run_comment_scrape)
         self.add_cascade(label="Actions", menu=self.actions_menu)
-        
+
         self.settingsmenu = Menu(self, tearoff=0)
         self.settingsmenu.add_command(label="Keywords", command=self._open_keywords_menu)
         self.settingsmenu.add_command(label="Context Size", command=self._open_context_size_menu)
         self.add_cascade(label="Settings", menu=self.settingsmenu)
-        
+
         self.add_command(label="Help", command=self._get_help)
 
     def _new_summary_form(self):
         self.summary_form = SummaryForm(self)
 
-    def _check_for_updates(self):
-        updating = check_for_updates()
-        if not updating:
-            messagebox.showinfo('Updates', 'You\'re up-to-date!')
-            return
-        self.parent.destroy()
-        
     def _open_keywords_menu(self):
         self.keyword_settings_form = KeywordSettingsForm(self)
 
@@ -645,18 +648,23 @@ class MenuBar(Menu):
     def set_app_status(self, message):
         self.parent.set_status(message)
 
+
 def findWholeWord(w):
     return re.compile(r'\b^({0})$\b'.format(w), flags=re.IGNORECASE).search
+
 
 def run_commentscraper():
     scrape_comments()
     return
 
+
 def on_enter(e):
     e.widget['background'] = '#ddd'
 
+
 def on_leave(e):
     e.widget['background'] = 'SystemButtonFace'
+
 
 def get_app_version():
     app_path = Path().absolute()
@@ -665,12 +673,14 @@ def get_app_version():
     version = config['Application']['version']
     return version
 
+
 def get_app_release():
     app_path = Path().absolute()
     config_path = app_path / "config/config.ini"
     config.read(config_path)
     release = datetime.strptime(config['Application']['release'], '%Y-%m-%dT%H:%M:%S')
     return release
+
 
 def center(win):
     """
@@ -689,19 +699,6 @@ def center(win):
     win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     win.deiconify()
 
-def check_for_updates():
-    r = requests.get('https://prestonneal.com/v1/apps/disabilitydude/latest')
-    response = r.json()
-    url = response['url']
-    release = get_app_release()
-    if release < datetime.strptime(response['release_date'], '%Y-%m-%dT%H:%M:%S'):
-        prompt_response = messagebox.askyesno('Update Available', f'A new update is available. Would you like to install now?')
-        if prompt_response == True:
-            app_path = Path().absolute()
-            subprocess.Popen(f'{app_path}\install.bat {url}',
-                creationflags=subprocess.CREATE_NEW_CONSOLE)
-            return True
-    return False
 
 if __name__ == '__main__':
     app = App()
